4d0172fe8c171fcec72c07abb276682db8352091
Rameez Hasan <rameeez110@gmail.com>
2022-04-01 12:24:09 +0500

    initial commit project created

---
+++ TollCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TollCalculator
+//
+//  Created by Rameez Hasan on 01/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
